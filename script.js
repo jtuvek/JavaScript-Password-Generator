7d386383622d899cc24ead4939958fac25e334d6
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-06-19 15:40:52 -0400

    Created more variables for characters

--- script.js
+++ script.js
@@ -22,4 +22,30 @@ function generatePassword() {
     alert("Please select at least one character type and specify the password length.");
     return "";
   }
+
+var password = "";
+var availableCharacters = "";
+
+if (characterTypes.includes("lowercase")) {
+  availableCharacters += "abcdefghijklmnopqrstuvwxyz";
+}
+
+if (characterTypes.includes("uppercase")) {
+  availableCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+
+if (characterTypes.includes("numeric")) {
+  availableCharacters += "0123456789";
+}
+
+if (characterTypes.includes("special")) {
+  availableCharacters += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+}
+
+for (var i = 0; i < passwordLength; i++) {
+  var randomIndex = Math.floor(Math.random() * availableCharacters.length);
+  password += availableCharacters.charAt(randomIndex);
+}
+
+return password;
 }
